25c8485f8329c57643a465035a615478e0a9cb60
Pyae Phyo Khant <146649651+pyaephyokhant760@users.noreply.github.com>
2024-06-22 14:49:56 +0630

    four commit

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -4,9 +4,12 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
@@ -20,10 +23,18 @@ class UserTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_if_user_with_specific_emial_in_database(): void
+    public function test_if_user_with_specific_email_in_database(): void
     {
-        $response = $this->post('/api/register');
+        $email = 'test@example.com';
+        $response = $this->post('/api/register', [
+            'name' => 'Test User',
+            'email' => $email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
 
-        dd($response);
+        $user = User::where('email', $email)->first();
+
     }
+
 }
